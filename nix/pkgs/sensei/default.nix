9a41664251734d6a4ee39dde2f54a96c867afd89
Jörg Thalheim <joerg@thalheim.io>
2022-09-23 12:48:39 +0200

    drop rust patch

--- nix/pkgs/sensei/default.nix
+++ nix/pkgs/sensei/default.nix
@@ -18,21 +18,14 @@ in rustPlatform.buildRustPackage {
     rustfmt
   ];
 
-  cargoPatches = [
-    # https://github.com/L2-Technology/sensei/pull/115
-    (fetchpatch {
-      url = "https://github.com/L2-Technology/sensei/commit/82ce4440a00ed43b0c81b084403ae949bb3ee3b5.patch";
-      sha256 = "sha256-vwO1LgzmM1oRc283XX2QQ/9zudAZNsf/TNqZE8j5O+Y=";
-    })
-  ];
-
   postPatch = ''
     mkdir -p web-admin/build
     cp -r ${web-admin}/* web-admin/build
   '';
+  # Disable bitcoind crate because it downloads a bitcoind binary at test time
   doCheck = false;
 
-  cargoSha256 = "sha256-bSkTC2jL3KGxwukbHmfSBHJxwJtlMrjrqNpOY4d6x/M=";
+  cargoSha256 = "sha256-X66spc6TzHfGUxqvtSYHtuMrHg3iiawJaxgnRWDRkCk=";
 
   passthru.updateScript = callPackage ./update.nix {};
 }
