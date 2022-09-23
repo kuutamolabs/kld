6b46c1ed18411a3d8e4f0f0a85ac28ba48ba81a8
Jörg Thalheim <joerg@thalheim.io>
2022-09-23 16:25:21 +0200

    add bitcoind package

--- nix/pkgs/flake-module.nix
+++ nix/pkgs/flake-module.nix
@@ -2,6 +2,8 @@
   perSystem = { pkgs, ... }: {
     packages = {
       sensei = pkgs.callPackage ./sensei { };
+      # Inspired by https://docs.lightning.engineering/lightning-network-tools/lnd/optimal-configuration-of-a-routing-node
+      bitcoind = pkgs.bitcoind.override { withWallet = false; };
     };
   };
 }
