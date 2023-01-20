ba2e96c3bc207222d819be953a42012788de891a
Jörg Thalheim <joerg@thalheim.io>
2023-01-20 16:52:30 +0000

    nixos: apply systemd hardening and wait for database to start

--- nix/modules/lightning-knd/default.nix
+++ nix/modules/lightning-knd/default.nix
@@ -1,5 +1,6 @@
 { config
 , lib
+, pkgs
 , ...
 }:
 let
@@ -38,21 +39,89 @@ in
   };
 
   imports = [
+    ../cockroachdb.nix
   ];
 
   config = {
     # for cli
     environment.systemPackages = [ cfg.package ];
 
+    services.cockroachdb.ensureDatabases = [ "lightning_knd" ];
+    services.cockroachdb.ensureUsers = [{
+      name = "lightning-knd";
+      ensurePermissions."DATABASE lightning_knd" = "ALL";
+    }];
+
     networking.firewall.allowedTCPPorts = lib.optionals cfg.openFirewall [ ];
+
+    users.users.lightning-knd = {
+      isSystemUser = true;
+      group = "lightning-knd";
+      extraGroups = [ "cockroachdb" ];
+    };
+    users.groups.lightning-knd = { };
 
     # fix me, we need to wait for the database to start first
     systemd.services.lightning-knd = {
+      wantedBy = [ "multi-user.target" ];
+      after = [ "network.target" "cockroachdb.service" ];
+      environment = {
+        KND_DATABASE_HOST = "/run/cockroachdb";
+        KND_DATABASE_PORT = "26257";
+        KND_DATABASE_USER = "lightning-knd";
+        KND_DATABASE_NAME = "lightning_knd";
+      };
+      path = [
+        config.services.cockroachdb.package
+      ];
       serviceConfig = {
-        DynamicUser = true;
         User = "lightning-knd";
         Group = "lightning-knd";
-        ExecStart = "${lib.getExe cfg.package}";
+        SupplementaryGroups = [ "cockroachdb" ];
+
+        # New file permissions
+        UMask = "0027"; # 0640 / 0750
+
+        # Hardening measures
+        # Sandboxing (sorted by occurrence in https://www.freedesktop.org/software/systemd/man/systemd.exec.html)
+
+        ProtectSystem = "full";
+        Type = "simple";
+        ProtectHome = true;
+        ProtectHostname = true;
+        ProtectClock = true;
+        ProtectKernelTunables = true;
+        ProtectKernelModules = true;
+        ProtectKernelLogs = true;
+        ProtectControlGroups = true;
+
+        NoNewPrivileges = true;
+        PrivateDevices = true;
+        PrivateTmp = true;
+        PrivateMounts = true;
+        MemoryDenyWriteExecute = true;
+        RemoveIPC = true;
+
+        Restart = "on-failure";
+
+        RestrictAddressFamilies = [ "AF_INET" "AF_INET6" "AF_UNIX" ];
+        RestrictRealtime = true;
+        RestrictSUIDSGID = true;
+
+        LockPersonality = true;
+
+        # Proc filesystem
+        ProcSubset = "pid";
+        ProtectProc = "invisible";
+
+        RestrictNamespaces = true;
+
+        SystemCallArchitectures = "native";
+        # blacklist some syscalls
+        SystemCallFilter = [ "~@cpu-emulation @debug @keyring @mount @obsolete @privileged @setuid" ];
+
+        ExecStartPre = "+${pkgs.acl}/bin/setfacl -m u:lightning-knd:rw /run/cockroachdb/.s.PGSQL.26257";
+        ExecStart = lib.getExe cfg.package;
       };
     };
   };
