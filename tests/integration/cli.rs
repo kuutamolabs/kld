1c29cffc1a97fbae831a23c91f2a9f627c40959f
Jörg Thalheim <joerg@thalheim.io>
2023-01-25 14:05:03 +0000

    integration tests: result if deserialize fails
    I had issues seeing the actual error or the data it could not parse.

--- tests/integration/cli.rs
+++ tests/integration/cli.rs
@@ -1,5 +1,6 @@
 use std::process::{Command, Output};
 
+use anyhow::{bail, Result};
 use api::{
     Channel, FundChannelResponse, GetInfo, NewAddressResponse, Peer, WalletBalance,
     WalletTransferResponse,
@@ -12,74 +13,83 @@ use crate::mocks::{TEST_ADDRESS, TEST_PUBLIC_KEY};
 use super::api::API_SETTINGS;
 
 #[test]
-fn test_cli_get_info() {
+fn test_cli_get_info() -> Result<()> {
     let output = run_cli("get-info", &[]);
-    let _: GetInfo = deserialize(&output.stdout);
+    let _: GetInfo = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_get_balance() {
+fn test_cli_get_balance() -> Result<()> {
     let output = run_cli("get-balance", &[]);
-    let _: WalletBalance = deserialize(&output.stdout);
+    let _: WalletBalance = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_new_address() {
+fn test_cli_new_address() -> Result<()> {
     let output = run_cli("new-address", &[]);
-    let _: NewAddressResponse = deserialize(&output.stdout);
+    let _: NewAddressResponse = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_withdraw() {
+fn test_cli_withdraw() -> Result<()> {
     let output = run_cli(
         "withdraw",
         &["--address", TEST_ADDRESS, "--satoshis", "1000"],
     );
-    let _: WalletTransferResponse = deserialize(&output.stdout);
+    let _: WalletTransferResponse = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_list_channels() {
+fn test_cli_list_channels() -> Result<()> {
     let output = run_cli("list-channels", &[]);
-    let _: Vec<Channel> = deserialize(&output.stdout);
+    let _: Vec<Channel> = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_list_peers() {
+fn test_cli_list_peers() -> Result<()> {
     let output = run_cli("list-peers", &[]);
-    let _: Vec<Peer> = deserialize(&output.stdout);
+    let _: Vec<Peer> = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_connect_peer() {
+fn test_cli_connect_peer() -> Result<()> {
     let output = run_cli("connect-peer", &["--public-key", TEST_PUBLIC_KEY]);
-    let _: PublicKey = deserialize(&output.stdout);
+    let _: PublicKey = deserialize(&output.stdout)?;
+    Ok(())
 }
 
 #[test]
-fn test_cli_disconnect_peer() {
+fn test_cli_disconnect_peer() -> Result<()> {
     let output = run_cli("disconnect-peer", &["--public-key", TEST_PUBLIC_KEY]);
-    let _: () = deserialize(&output.stdout);
+
+    deserialize(&output.stdout)
 }
 
 #[test]
-fn test_cli_open_channel() {
+fn test_cli_open_channel() -> Result<()> {
     let output = run_cli(
         "open-channel",
         &["--public-key", TEST_PUBLIC_KEY, "--satoshis", "1000"],
     );
-    let _: FundChannelResponse = deserialize(&output.stdout);
+    let _: FundChannelResponse = deserialize(&output.stdout)?;
+    Ok(())
 }
 
-fn deserialize<'a, T>(bytes: &'a [u8]) -> T
+fn deserialize<'a, T>(bytes: &'a [u8]) -> Result<T>
 where
     T: de::Deserialize<'a>,
 {
     match serde_json::from_slice::<T>(bytes) {
-        Ok(t) => t,
+        Ok(t) => Ok(t),
         Err(e) => {
-            println!("{}", String::from_utf8(bytes.to_owned()).unwrap());
-            panic!("{}", e);
+            let s = String::from_utf8_lossy(bytes);
+            bail!("cannot parse '{}' as json: {}", s, e)
         }
     }
 }
