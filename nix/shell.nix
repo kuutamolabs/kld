8cb4f29eb78f168b0120ff179075b8308e1807c7
Jörg Thalheim <joerg@thalheim.io>
2023-01-27 09:38:10 +0000

    re-add rustc to devshell
    crane does not provide this.

--- nix/shell.nix
+++ nix/shell.nix
@@ -7,7 +7,7 @@
     }:
     {
       devShells.default = pkgs.mkShell {
-        buildInputs = [
+        packages = [
           # code formatting
           config.packages.treefmt
 
@@ -20,7 +20,11 @@
           pkgs.rust-analyzer
           pkgs.cargo-watch
           pkgs.clippy
-        ] ++ self'.packages.lightning-knd.buildInputs;
+
+          # crane does not have this in nativeBuildInputs
+          pkgs.rustc
+        ] ++ config.packages.lightning-knd.nativeBuildInputs;
+        inherit (config.packages.lightning-knd) buildInputs;
         RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
         RUST_BACKTRACE = 1;
         inherit (self'.packages.lightning-knd) nativeBuildInputs;
