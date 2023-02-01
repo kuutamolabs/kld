843bca0db186cd28dba5eb9af0993688d99cd46e
Joseph Goulden <joegoulden@protonmail.com>
2023-02-01 11:47:04 +0000

    test: use result for prometheus tests

--- tests/integration/prometheus.rs
+++ tests/integration/prometheus.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use futures::FutureExt;
 use std::sync::Arc;
 use test_utils::test_settings;
@@ -7,7 +7,7 @@ use crate::{mocks::mock_lightning::MockLightning, quit_signal};
 use lightning_knd::prometheus::start_prometheus_exporter;
 
 #[tokio::test(flavor = "multi_thread")]
-pub async fn test_prometheus() {
+pub async fn test_prometheus() -> Result<()> {
     let address = test_settings().exporter_address.clone();
     println!("ADDRESS: {}", address);
 
@@ -24,33 +24,34 @@ pub async fn test_prometheus() {
         quit_signal().shared(),
     ));
 
-    let health = call_exporter(&address, "health").await.unwrap();
+    let health = call_exporter(&address, "health").await?;
     assert_eq!(health, "OK");
 
-    let pid = call_exporter(&address, "pid").await.unwrap();
+    let pid = call_exporter(&address, "pid").await?;
     assert_eq!(pid, std::process::id().to_string());
 
-    let result = call_exporter(&address, "metrics").await.unwrap();
-    assert!(get_metric(&result, "lightning_knd_uptime") > 0.0);
+    let result = call_exporter(&address, "metrics").await?;
+    assert!(get_metric(&result, "lightning_knd_uptime")?.is_finite());
     assert_eq!(
-        get_metric(&result, "lightning_node_count"),
+        get_metric(&result, "lightning_node_count")?,
         metrics.num_nodes as f64
     );
     assert_eq!(
-        get_metric(&result, "lightning_channel_count"),
+        get_metric(&result, "lightning_channel_count")?,
         metrics.num_channels as f64
     );
     assert_eq!(
-        get_metric(&result, "lightning_peer_count"),
+        get_metric(&result, "lightning_peer_count")?,
         metrics.num_peers as f64
     );
     assert_eq!(
-        get_metric(&result, "wallet_balance"),
+        get_metric(&result, "wallet_balance")?,
         metrics.wallet_balance as f64
     );
 
-    let not_found = call_exporter(&address, "wrong").await.unwrap();
+    let not_found = call_exporter(&address, "wrong").await?;
     assert_eq!(not_found, "Not Found");
+    Ok(())
 }
 
 async fn call_exporter(address: &str, method: &str) -> Result<String, reqwest::Error> {
@@ -60,14 +61,13 @@ async fn call_exporter(address: &str, method: &str) -> Result<String, reqwest::E
         .await
 }
 
-fn get_metric(metrics: &str, name: &str) -> f64 {
-    metrics
+fn get_metric(metrics: &str, name: &str) -> Result<f64> {
+    Ok(metrics
         .lines()
         .find(|x| x.starts_with(name))
-        .unwrap()
+        .with_context(|| "Metric not found")?
         .split(' ')
         .last()
-        .unwrap()
-        .parse::<f64>()
-        .unwrap()
+        .with_context(|| "Bad metric format")?
+        .parse::<f64>()?)
 }
