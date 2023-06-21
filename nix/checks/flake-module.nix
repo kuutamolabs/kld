a9ec9f02fab9af2e98a6015d351a5926a9edd861
Jörg Thalheim <joerg@thalheim.io>
2023-06-21 03:00:19 +0000

    Revert "check if we can make ci work by spawning less tests"
    This reverts commit 366344594d000b72888867ef72aa1c1342339f89.

--- nix/checks/flake-module.nix
+++ nix/checks/flake-module.nix
@@ -3,7 +3,7 @@
     checks = {
       kld-clippy = config.packages.kld.clippy;
       kld-benches = config.packages.kld.benches;
-      #kld-tests = config.packages.kld.tests;
+      kld-tests = config.packages.kld.tests;
 
       kld-mgr-clippy = config.packages.kld-mgr.clippy;
       kld-mgr-tests = config.packages.kld-mgr.tests;
