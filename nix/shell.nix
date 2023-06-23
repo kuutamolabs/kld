3174b16df2400db62267c0f875dc7ff637e0ae00
Antonio Yang <yanganto@gmail.com>
2023-06-23 14:47:16 +0800

    add nixos-anywhere (#415)

--- nix/shell.nix
+++ nix/shell.nix
@@ -2,12 +2,15 @@
   perSystem =
     { config
     , self'
+    , inputs'
     , pkgs
     , ...
     }:
     {
       devShells.default = pkgs.mkShell {
         packages = [
+          inputs'.nixos-anywhere.packages.nixos-anywhere
+
           # code formatting
           config.treefmt.build.wrapper
 
