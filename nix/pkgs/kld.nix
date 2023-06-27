a063312b149182f043de8461fce58ae2034dfa9f
Antonio Yang <yanganto@gmail.com>
2023-06-27 14:04:01 +0800

    remove symlink in upstream

--- nix/pkgs/kld.nix
+++ nix/pkgs/kld.nix
@@ -5,7 +5,6 @@
 , bitcoind
 , cockroachdb
 , pkg-config
-, rsync
 , self
 }:
 let
@@ -26,43 +25,29 @@ let
   buildInputs = [ openssl ];
   nativeBuildInputs = [ pkg-config ];
   cargoExtraArgs = "--workspace --all-features";
-  # this is a bit of an hack, since we have to copy the vendor dir and find the broken symlink and replace it with the real file
-  # we should remove (or disable) this if pointing to a stable release again
-  cargoVendorDir = (craneLib.vendorCargoDeps { inherit src; }).overrideAttrs (old: {
-    buildCommand = ''
-      env >&2
-      ${old.buildCommand}
-      mv "$out" broken
-      ${rsync}/bin/rsync -a --copy-unsafe-links --chmod=u=rwX broken/ "$out/"
-      src=$(find "$out" -type f -wholename '*util/time.rs')
-      dst=$(find "$out" -type l -name 'time_utils.rs')
-      rm -f "$dst"
-      cp -f "$src" "$dst"
-    '';
-  });
   cargoArtifacts = craneLib.buildDepsOnly {
-    inherit src cargoToml buildInputs nativeBuildInputs cargoExtraArgs cargoVendorDir;
+    inherit src cargoToml buildInputs nativeBuildInputs cargoExtraArgs;
   };
 in
 craneLib.buildPackage {
   name = "kld";
-  inherit src cargoToml cargoArtifacts buildInputs nativeBuildInputs cargoVendorDir;
+  inherit src cargoToml cargoArtifacts buildInputs nativeBuildInputs;
   cargoExtraArgs = "${cargoExtraArgs} --bins --examples --lib";
   passthru = {
     clippy = craneLib.cargoClippy {
-      inherit src cargoToml cargoArtifacts buildInputs nativeBuildInputs cargoExtraArgs cargoVendorDir;
+      inherit src cargoToml cargoArtifacts buildInputs nativeBuildInputs cargoExtraArgs;
       cargoClippyExtraArgs = "--all-targets --no-deps -- -D warnings";
     };
     benches = craneLib.mkCargoDerivation {
-      inherit src cargoToml cargoArtifacts buildInputs nativeBuildInputs cargoExtraArgs cargoVendorDir;
+      inherit src cargoToml cargoArtifacts buildInputs nativeBuildInputs cargoExtraArgs;
       buildPhaseCargoCommand = "cargo bench --no-run";
     };
     # having the tests seperate avoids having to run them on every package change.
     tests = craneLib.cargoTest {
-      inherit src cargoToml cargoArtifacts buildInputs cargoExtraArgs cargoVendorDir;
+      inherit src cargoToml cargoArtifacts buildInputs cargoExtraArgs;
       nativeBuildInputs = nativeBuildInputs ++ [ bitcoind cockroachdb ];
     };
-    inherit cargoArtifacts cargoVendorDir;
+    inherit cargoArtifacts;
   };
 
   # we run tests in a seperate package
