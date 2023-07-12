78b05ac003e30b84e675d6f5ff81274e5c1e3f8d
paulmattei <paul@kuutamo.co>
2023-07-12 13:49:49 +0000

    remove message

--- mgr/src/config.rs
+++ mgr/src/config.rs
@@ -448,7 +448,7 @@ fn validate_host(name: &str, host: &HostConfig, default: &HostConfig) -> Result<
         }
         // FIXME: this is currently an unstable feature
         //if address.is_global() {
-        //    warn!("ipv4_address provided for hosts.{} is not a public ipv4 address: {}. This might not work with near mainnet", name, address);
+        //    warn!("ipv4_address provided for hosts.{} is not a public ipv4 address: {}.", name, address);
         //}
         Some(address)
     } else {
@@ -502,7 +502,7 @@ fn validate_host(name: &str, host: &HostConfig, default: &HostConfig) -> Result<
 
         // FIXME: this is currently an unstable feature
         //if ipv6_address.is_global() {
-        //    warn!("ipv6_address provided for hosts.{} is not a public ipv6 address: {}. This might not work with near mainnet", name, ipv6_address);
+        //    warn!("ipv6_address provided for hosts.{} is not a public ipv6 address: {}.", name, ipv6_address);
         //}
 
         (Some(ipv6_address), mask)
@@ -739,7 +739,7 @@ fn decode_token(s: String) -> Result<(String, String)> {
 #[cfg(test)]
 pub(crate) const TEST_CONFIG: &str = r#"
 [global]
-flake = "github:myfork/near-staking-knd"
+flake = "github:myfork/lightning-knd"
 
 [host_defaults]
 public_ssh_keys = [
@@ -773,7 +773,7 @@ pub fn test_parse_config() -> Result<()> {
     use std::str::FromStr;
 
     let config = parse_config(TEST_CONFIG, Path::new("/"))?;
-    assert_eq!(config.global.flake, "github:myfork/near-staking-knd");
+    assert_eq!(config.global.flake, "github:myfork/lightning-knd");
 
     let hosts = &config.hosts;
     assert_eq!(hosts.len(), 3);
