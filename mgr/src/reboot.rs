ab0b62c8f08f395798bb5c79c79fdb3b0570adc1
Antonio Yang <yanganto@gmail.com>
2023-08-21 18:28:01 +0800

    sperate reboot and unlock

--- mgr/src/reboot.rs
+++ mgr/src/reboot.rs
@@ -1,11 +1,10 @@
 use anyhow::Result;
-use std::path::PathBuf;
 
 use super::ssh;
 use super::Host;
 use log::info;
 
-pub fn reboot(hosts: &[Host], disk_encryption_key: Option<PathBuf>) -> Result<()> {
+pub fn reboot(hosts: &[Host]) -> Result<()> {
     ssh(hosts, &["nohup reboot &>/dev/null & exit"])?;
     info!("Note unlock is required after node reboot");
     Ok(())
