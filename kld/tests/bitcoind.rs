66271f800510b911a4fb738fd07cd909d8bc9b9e
Antonio Yang <yanganto@gmail.com>
2023-09-07 16:07:05 +0800

    refactor bitcoind_manager

--- kld/tests/bitcoind.rs
+++ kld/tests/bitcoind.rs
@@ -1,6 +1,6 @@
 use std::str::FromStr;
 
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{anyhow, bail, Result};
 use bitcoin::Address;
 use kld::bitcoind::bitcoind_interface::BitcoindInterface;
 use lightning::chain::chaininterface::{ConfirmationTarget, FeeEstimator};
@@ -13,50 +13,59 @@ pub async fn test_bitcoind_client() -> Result<()> {
 
     let mut settings = test_settings(&tmp_dir, "client");
     let bitcoind = BitcoinManager::new(&tmp_dir, &mut settings).await?;
-    let client = bitcoind.client.get().context("expected client")?;
     let n_blocks = 3;
-    client
+    bitcoind
+        .client
         .generate_to_address(n_blocks, &Address::from_str(TEST_ADDRESS).unwrap())
         .await?;
 
-    client.wait_for_blockchain_synchronisation().await;
+    bitcoind.client.wait_for_blockchain_synchronisation().await;
 
-    let info = client.get_blockchain_info().await?;
+    let info = bitcoind.client.get_blockchain_info().await?;
     assert_eq!(n_blocks, info.blocks);
 
-    let best_block = client
+    let best_block = bitcoind
+        .client
         .get_best_block()
         .await
         .map_err(|e| anyhow!(e.into_inner()))?;
     assert_eq!(best_block.0, info.best_block_hash);
     assert_eq!(best_block.1, Some(n_blocks as u32));
 
-    let header = client
+    let header = bitcoind
+        .client
         .get_header(&best_block.0, None)
         .await
         .map_err(|e| anyhow!(e.into_inner()))?;
     assert_eq!(header.height, n_blocks as u32);
     assert_eq!(header.chainwork.low_u64(), 8);
 
-    let block = &client
+    let block = &bitcoind
+        .client
         .get_block(&best_block.0)
         .await
         .map_err(|e| anyhow!(e.into_inner()))?;
 
     assert_eq!(
         2000,
-        client.get_est_sat_per_1000_weight(ConfirmationTarget::Background)
+        bitcoind
+            .client
+            .get_est_sat_per_1000_weight(ConfirmationTarget::Background)
     );
     assert_eq!(
         5000,
-        client.get_est_sat_per_1000_weight(ConfirmationTarget::Normal)
+        bitcoind
+            .client
+            .get_est_sat_per_1000_weight(ConfirmationTarget::Normal)
     );
     assert_eq!(
         10000,
-        client.get_est_sat_per_1000_weight(ConfirmationTarget::HighPriority)
+        bitcoind
+            .client
+            .get_est_sat_per_1000_weight(ConfirmationTarget::HighPriority)
     );
 
-    client.poll_for_fee_estimates();
+    bitcoind.client.poll_for_fee_estimates();
 
     match block {
         BlockData::FullBlock(block) => assert_eq!(block.block_hash(), best_block.0),
