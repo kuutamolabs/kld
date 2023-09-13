1143e620fc7ac811393c11452457a1546dc9a5a7
Antonio Yang <yanganto@gmail.com>
2023-09-13 21:14:38 +0800

    keep systemd log for update

--- nix/modules/ctl/default.nix
+++ nix/modules/ctl/default.nix
@@ -1,6 +1,7 @@
 { config, lib, self, ... }:
 let
   cfg = config.kuutamo.ctl;
+  systemd = config.systemd.package;
 in
 {
   options.kuutamo.ctl = {
@@ -13,6 +14,9 @@ in
   };
 
   config = {
+    system.activationScripts.node-upgrade = ''
+      ${systemd}/bin/systemd-run --collect --unit nixos-upgrade echo level=info message=\"kld node updated\" $(${cfg.package}/bin/kld-ctl system-info --inline)
+    '';
     environment.systemPackages = [ cfg.package ];
     environment.etc."system-info.toml".text = lib.mkDefault ''
       git_sha = "${self.rev or "dirty"}"
