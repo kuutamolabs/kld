082c4b337eac2399defe64d6d9a95b7798e0a0e6
Antonio Yang <yanganto@gmail.com>
2023-10-13 19:55:44 +0800

    update crane

--- flake.nix
+++ flake.nix
@@ -16,9 +16,7 @@
     rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
     rust-overlay.inputs.flake-utils.follows = "flake-utils";
 
-    # allow offline evaluation for git dependencies, see https://github.com/ipetkov/crane/pull/266
-    #crane.url = "github:ipetkov/crane";
-    crane.url = "github:Mic92/crane/allow-output-hashes";
+    crane.url = "github:ipetkov/crane";
     crane.inputs.nixpkgs.follows = "nixpkgs";
     crane.inputs.flake-utils.follows = "flake-utils";
     crane.inputs.rust-overlay.follows = "rust-overlay";
