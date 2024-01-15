e9509f739e5fc18d1e8d3421b73e80003757fe20
Antonio Yang <yanganto@gmail.com>
2024-01-15 10:30:42 +0800

    update dep hash

--- nix/pkgs/kld-tui.nix
+++ nix/pkgs/kld-tui.nix
@@ -24,9 +24,9 @@ let
   nativeBuildInputs = [ pkg-config ];
   cargoExtraArgs = "--workspace --all-features";
   outputHashes = {
-    "git+https://github.com/kuutamolabs/bdk?branch=0.28.2-allow-begin-match-fail#198c698c6c97055fc3f88f8af92f42a45ef709eb" = "sha256-jDYkAS1u6yl5MgBaPUS+RIcf47vCdnBsRS9/IHeS6yI=";
+    "git+https://github.com/kuutamolabs/bdk?branch=0.29.0-allow-begin-match-fail#d983732c7e290caff336a39981b6159b4c44c22e" = "sha256-63rvs64cGW2JzYWQQRMhIM5hILBpGjdsR8VTpjHuNPE=";
     "git+https://github.com/hyperium/mime#938484de95445a2af931515d2b7252612c575da7" = "sha256-Zdhw4wWK2ZJrv62YoJMdTHaQhIyKxtG2UCu/m3mQwy0=";
-    "git+https://github.com/kuutamolabs/ldk-lsp-client?branch=fix-cyc-dep#2a9c244643969a450a71a70fc918b1124f1c6bd1" = "sha256-2VmHHxgCK3Bcz+Nb9+j2hxoetgWff32ZuWWU6rd8PFw=";
+    "git+https://github.com/kuutamolabs/ldk-lsp-client?branch=kuutamo-0.0.119#5e0076ca97a30cd02402af78f914eb7de32e1a2a" = "sha256-bIRM2qS7b3eoeCtEcw0eIRK/YpidKQ3bR9ueHR8yocc=";
     "git+https://github.com/yanganto/ratatui?branch=table-footer#5268fd23cc85d8043335f74262b3fda729ca9750" = "sha256-j2rzlIZBF4mKLfIoNfBYAUjeBKYPJzJcnON6X8A7jbw=";
   };
   cargoArtifacts = craneLib.buildDepsOnly {
